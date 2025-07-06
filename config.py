a8bb7b1bdb4358d81522fe61aa9230ecd5fb5b59
ali-akbar7700 <217789695+ali-akbar7700@users.noreply.github.com>
2025-07-06 15:55:31 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import requests
+# config.py
+BINANCE_API_URL = "https://api.binance.com"
+import os
 
-API_TOKEN = "7701492103:AAHzyTt77VQEhTUfBnsEl7qGkzFI5KfYUiQ"
-
-response = requests.get(f"https://api.telegram.org/bot{API_TOKEN}/getMe")
-print(response.status_code)
-print(response.json())
+BOT_TOKEN = os.getenv("7701492103:AAHzyTt77VQEhTUfBnsEl7qGkzFI5KfYUiQ")
+GPT_API_KEY = os.getenv("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")
+DOLLAR_RATE = int(os.getenv("DOLLAR_RATE", 91600))  # اگر نبود 60000 استفاده می‌کنه
